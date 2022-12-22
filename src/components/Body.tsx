5e167fdf675921ee880e8122a38519e16c4d9233
Joao-Neto-DH <joaolima882018@gmail.com>
2022-12-22 09:10:34 +0100

    Criando Body Component

---
+++ src/components/Body.tsx
@@ -0,0 +1,12 @@
+import "./Body.css";
+
+function Body() {
+    return ( 
+        <main className="main-content">
+            <div>smaple1</div>
+            <div>sample2</div>
+        </main> 
+    );
+}
+
+export default Body;
