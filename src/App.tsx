a0b717cd826b661093180e6f029537567a8338d2
Joao-Neto-DH <joaolima882018@gmail.com>
2023-01-01 11:30:27 +0100

    Instalando react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
-import Ad from './components/Ad';
-import ChatRoom from './components/ChatRoom';
-import HomePage from './pages/HomePage';
-import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 
   return (
     <>
-      <MainPage>
-        
-        <HomePage></HomePage>
-      </MainPage>
+      <LoginPage/>
     </>
   );
 }
