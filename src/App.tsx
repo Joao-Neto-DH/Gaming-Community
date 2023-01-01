f8932dedc186b9f89f9fd27ff5ff01b2319996c2
Joao-Neto-DH <joaolima882018@gmail.com>
2023-01-01 11:57:39 +0100

    Criando a rota inicial '/'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 //import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import MainPage from './pages/MainPage';
+
+const route = createBrowserRouter([
+  {
+    path:"/",
+    element: <MainPage>d</MainPage>
+  }
+]);
+
 
 function App() {
 
   return (
     <>
-      <LoginPage/>
+      
     </>
   );
 }
 
-export default App;
+export { App, RouterProvider, route };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { route, RouterProvider } from './App';
 import reportWebVitals from './reportWebVitals';
-import ContextComponent from './sample/context/ContextComponent';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,8 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    {/* <ContextComponent/> */}
-    <App />
+    <RouterProvider router={route}/>
   </React.StrictMode>
 );
 
