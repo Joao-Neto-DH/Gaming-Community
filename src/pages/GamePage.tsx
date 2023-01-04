03f047923fe747b63233883d0a24801ff17f8f1d
Joao-Neto-DH <joaolima882018@gmail.com>
2023-01-04 15:09:59 +0100

    Adicionando formulário de comentário

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -49,75 +49,87 @@ function GamePage() {
                             </>
                         </Container>
                     </aside>
-                    <Container style={{width:"70%",alignSelf:"flex-start", marginLeft:"auto", marginRight:"auto"}}>
-                        <>
-                            <div className="game-imgs">
-                                <img key={0} src="/assets/img/gow.jpg" alt="gow" className="game-img" />
-                                <img key={1} src="/assets/img/fortnite.jpg" alt="gow" className="game-img" />
-                                <img key={2} src="/assets/img/mario.jpg" alt="gow" className="game-img" />
-                            </div>
-                            <Separator/>
-                            <div className="details box">
-                                <div>
-                                    <Title2>
-                                        Plataformas
-                                    </Title2>
-                                    <ul>
-                                        <li key={0}>
-                                            💻 Windows 7/8/8.1/10/11
-                                        </li>
-                                        <li key={1}>
-                                            📱 Android 8
-                                        </li>
-                                    </ul>
+                    <main style={{width:"70%",alignSelf:"flex-start", marginLeft:"auto", marginRight:"auto"}}>
+                        <Container >
+                            <>
+                                <div className="game-imgs">
+                                    <img key={0} src="/assets/img/gow.jpg" alt="gow" className="game-img" />
+                                    <img key={1} src="/assets/img/fortnite.jpg" alt="gow" className="game-img" />
+                                    <img key={2} src="/assets/img/mario.jpg" alt="gow" className="game-img" />
                                 </div>
-                                <div>
-                                    <Title2>
-                                        Requesitos
-                                    </Title2>
-                                    <ul>
-                                        <li key={3}>Computador: i5 @3.0Ghz</li>
-                                        <li key={4}>Placa gráfica: AMD Radeon HD - 1GB</li>
-                                        <li key={5}>Ram: 8GB</li>
+                                <Separator/>
+                                <div className="details box">
+                                    <div>
+                                        <Title2>
+                                            Plataformas
+                                        </Title2>
+                                        <ul>
+                                            <li key={0}>
+                                                💻 Windows 7/8/8.1/10/11
+                                            </li>
+                                            <li key={1}>
+                                                📱 Android 8
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <Title2>
+                                            Requesitos
+                                        </Title2>
+                                        <ul>
+                                            <li key={3}>Computador: i5 @3.0Ghz</li>
+                                            <li key={4}>Placa gráfica: AMD Radeon HD - 1GB</li>
+                                            <li key={5}>Ram: 8GB</li>
 
-                                    </ul>
-                                </div>
-                                <div>
-                                    <Title2>
-                                        Recomendado
-                                    </Title2>
-                                    <ul>
-                                        <li key={3}>Computador: i5 @3.0Ghz</li>
-                                        <li key={4}>Placa gráfica: AMD Radeon HD - 1GB</li>
-                                        <li key={5}>Ram: 8GB</li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <Title2>
+                                            Recomendado
+                                        </Title2>
+                                        <ul>
+                                            <li key={3}>Computador: i5 @3.0Ghz</li>
+                                            <li key={4}>Placa gráfica: AMD Radeon HD - 1GB</li>
+                                            <li key={5}>Ram: 8GB</li>
 
-                                    </ul>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <Separator />
-                            <div className="box" style={{display:"flex",flexDirection:"column"}}>
-                                <Title2>
-                                    Descrição
-                                </Title2>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores laborum quas nesciunt nihil, praesentium aliquam mollitia perferendis. Maxime eaque sunt, dolorem rem architecto doloremque repellat tempore distinctio? Alias, aperiam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, rem minima! Perspiciatis officia veritatis dolorem. Sit nam voluptates, eaque dolor esse repellat magni repellendus perferendis laboriosam id nihil! Mollitia, ea?
-                                </p>
-                                <figure className="img-text">
-                                    <img src="/assets/img/gow.jpg" alt="gow" />
-                                    <figcaption>Fonte: GameVicio</figcaption>
-                                </figure>
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores laborum quas nesciunt nihil, praesentium aliquam mollitia perferendis. Maxime eaque sunt, dolorem rem architecto doloremque repellat tempore distinctio? Alias, aperiam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, rem minima! Perspiciatis officia veritatis dolorem. Sit nam voluptates, eaque dolor esse repellat magni repellendus perferendis laboriosam id nihil! Mollitia, ea?
-                                </p>
-                            </div>
-                        </>
-                    </Container>
+                                <Separator />
+                                <div className="box" style={{display:"flex",flexDirection:"column"}}>
+                                    <Title2>
+                                        Descrição
+                                    </Title2>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores laborum quas nesciunt nihil, praesentium aliquam mollitia perferendis. Maxime eaque sunt, dolorem rem architecto doloremque repellat tempore distinctio? Alias, aperiam.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, rem minima! Perspiciatis officia veritatis dolorem. Sit nam voluptates, eaque dolor esse repellat magni repellendus perferendis laboriosam id nihil! Mollitia, ea?
+                                    </p>
+                                    <figure className="img-text">
+                                        <img src="/assets/img/gow.jpg" alt="gow" />
+                                        <figcaption>Fonte: GameVicio</figcaption>
+                                    </figure>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores laborum quas nesciunt nihil, praesentium aliquam mollitia perferendis. Maxime eaque sunt, dolorem rem architecto doloremque repellat tempore distinctio? Alias, aperiam.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, rem minima! Perspiciatis officia veritatis dolorem. Sit nam voluptates, eaque dolor esse repellat magni repellendus perferendis laboriosam id nihil! Mollitia, ea?
+                                    </p>
+                                </div>
+                            </>
+                        </Container>
+
+                        <Container style={{marginTop: "4rem", padding:"2rem"}}>
+                            <>
+                                <Title2>Comentários</Title2>
+                                <Form className="comment-form">
+                                    <textarea name="commentary" id="commentary" rows={10} required></textarea>
+                                    <button type="submit" className="btn">Comentar</button>
+                                </Form>
+                            </>
+                        </Container>
+                    </main>
                 </>
             </Main>
         </>
